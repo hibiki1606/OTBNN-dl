036db61f07c7504e5b4dbd68eeac9a1882e1c556
Shewiiii <akiempj123@gmail.com>
2025-05-22 23:50:43 +0200

    Fix f-string error on older version of Python, minor refactor

--- otbnn_client.py
+++ otbnn_client.py
@@ -60,7 +60,7 @@ class BnnClient:
             return None
 
     async def get_posts_from_user(self, user_uuid: str, deep: bool) -> list[BnnPost]:
-        page_url = f"{self.base_api_url}/users/{user_uuid}/casts?is_adult={('true' if deep else 'false')}"
+        page_url = f"{self.base_api_url}/users/{user_uuid}/casts?is_adult={str(deep).lower()}"
         posts: list[BnnPost] = []
 
         post_count: int = 1
